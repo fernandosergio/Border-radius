28526dcda97a1c08e63c430d8d27f27a3d463f25
fyzn <46656725+fyzn@users.noreply.github.com>
2020-08-22 21:59:24 -0300

    funcoes textotanana

--- script.js
+++ script.js
@@ -8,24 +8,10 @@ let con = window.document.getElementById('confirmado')
 
 // Div e inputs type="checkbox" para comppatibilidade
 var botoes = window.document.querySelector('div#compatibilidade')
-let caixas = ''
+let caixas = document.querySelectorAll('input[type="checkbox"]')
 botoes.addEventListener('change', () => {
     caixas = document.querySelectorAll('input[type="checkbox"]')
-        /*if (caixas[0].checked == true) {
-            console.log('opa webkit')
-        } else {
-            console.log('não opa webkit')
-        }
-        if (caixas[1].checked == true) {
-            console.log('opa gecko')
-        } else {
-            console.log('não opa gecko')
-        }
-        if (caixas[2].checked == true) {
-            console.log('opa css')
-        } else {
-            console.log('não opa css')
-        }*/
+    raio()
 })
 
 // Função para copiar e colar do textarea
@@ -45,30 +31,35 @@ function raio() {
     let sd = Number(inputs[1].value)
     let ie = Number(inputs[2].value)
     let id = Number(inputs[3].value)
-    console.log(se, sd, ie, id)
 
-    if (se === sd && se === ie && se === id) {
-        texto.innerHTML = ''
-
-
-
-        atualiza1(se)
-    } else {
-        texto.innerHTML = ''
-        atualiza4(se, sd, id, ie)
+    texto.innerHTML = ''
+    if (caixas[0].checked == true) {
+        textoWebkit(se, sd, ie, id)
     }
+    if (caixas[1].checked == true) {
+        textoGecko(se, sd, ie, id)
+    }
+    if (caixas[2].checked == true) {
+        textoCss(se, sd, ie, id)
+    }
+    atualiza(se, sd, id, ie)
 }
 
 function textoWebkit(se, sd, ie, id) {
-    if (se === && se === ie && se === id) {
+    if (se === id && sd == ie) {
+        texto.innerHTML += `-webkit-border-radius: ${se}px ${sd}px;&#013;`
+    } else if (se === sd && se === ie && se === id) {
         texto.innerHTML += `-webkit-border-radius: ${se}px;&#013;`
     } else {
         texto.innerHTML += `-webkit-border-radius: ${se}px ${sd}px ${id}px ${ie}px;&#013;`
     }
 }
 
+
 function textoGecko(se, sd, ie, id) {
-    if (se === sd && se === ie && se === id) {
+    if (se === id && sd == ie) {
+        texto.innerHTML += `-moz-border-radius: ${se}px ${sd}px;&#013;`
+    } else if (se === sd && se === ie && se === id) {
         texto.innerHTML += `-moz-border-radius: ${se}px;&#013;`
     } else {
         texto.innerHTML += `-moz-border-radius: ${se}px ${sd}px ${id}px ${ie}px;&#013;`
@@ -76,7 +67,9 @@ function textoGecko(se, sd, ie, id) {
 }
 
 function textoCss(se, sd, ie, id) {
-    if (se === sd && se === ie && se === id) {
+    if (se === id && sd == ie) {
+        texto.innerHTML += `border-radius: ${se}px ${sd}px;&#013;`
+    } else if (se === sd && se === ie && se === id) {
         texto.innerHTML += `border-radius: ${se}px;&#013;`
     } else {
         texto.innerHTML += `border-radius: ${se}px ${sd}px ${id}px ${ie}px;&#013;`
@@ -84,7 +77,7 @@ function textoCss(se, sd, ie, id) {
 }
 
 // Função que atualiza as bordas
-function atualiza4(se = 0, sd = 0, id = 0, ie = 0) {
+function atualiza(se = 0, sd = 0, id = 0, ie = 0) {
     sec.style.borderRadius = `${se}px ${sd}px ${id}px ${ie}px`
     botao.style.backgroundColor = '#cecece'
     con.innerHTML = ''
