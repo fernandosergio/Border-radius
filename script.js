b96648d95b015c7192847a16019803b0abdd91e0
fyzn <46656725+fyzn@users.noreply.github.com>
2020-08-22 22:17:15 -0300

    comentarios

--- script.js
+++ script.js
@@ -32,6 +32,7 @@ function raio() {
     let ie = Number(inputs[2].value)
     let id = Number(inputs[3].value)
 
+    // Reseta o texto do textarea e verifica qual checkbox está ativo e aciona sua função especifica
     texto.innerHTML = ''
     if (caixas[0].checked == true) {
         textoWebkit(se, sd, ie, id)
@@ -42,20 +43,26 @@ function raio() {
     if (caixas[2].checked == true) {
         textoCss(se, sd, ie, id)
     }
+
+    // Atualiza os valores dos border radius
     atualiza(se, sd, id, ie)
 }
 
+// Função para mostrar webkit
 function textoWebkit(se, sd, ie, id) {
     if (se === sd && se === ie && se === id) {
+        // Caso todos os valores forem iguais
         texto.innerHTML += `-webkit-border-radius: ${se}px;&#013;`
     } else if (se === id && sd == ie) {
+        // Caso os valores sup esq igual ao inf dir e sup dir igual ao inf esq
         texto.innerHTML += `-webkit-border-radius: ${se}px ${sd}px;&#013;`
     } else {
+        // Caso todos sejam diferentes
         texto.innerHTML += `-webkit-border-radius: ${se}px ${sd}px ${id}px ${ie}px;&#013;`
     }
 }
 
-
+// Função para mostrar gecko
 function textoGecko(se, sd, ie, id) {
     if (se === sd && se === ie && se === id) {
         texto.innerHTML += `-moz-border-radius: ${se}px;&#013;`
@@ -66,6 +73,7 @@ function textoGecko(se, sd, ie, id) {
     }
 }
 
+// função para mostrar css3
 function textoCss(se, sd, ie, id) {
     if (se === sd && se === ie && se === id) {
         texto.innerHTML += `border-radius: ${se}px;&#013;`
